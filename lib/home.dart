4080fe5f318d92a7fc3118a7bfccae5201e0a0eb
HoverFo <hoverfox23@gmail.com>
2023-01-11 13:54:49 +0100

    Map and design

--- lib/home.dart
+++ lib/home.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    backgroundColor: Color(0xFF0386E8F);
     final ButtonStyle style = TextButton.styleFrom(
       // Changer couleur bouton ou police
     );
@@ -25,8 +28,68 @@ class MyHomePage extends StatelessWidget {
         ],
       ),
       body: Center(
-        child:Text("Home page",textScaleFactor: 2,)
-       ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Container(
+              width: 350.0,
+              height: 325.0,
+              decoration: BoxDecoration(
+                color: Color(0xFF001420),
+                borderRadius: BorderRadius.all(Radius.circular(20.0)),
+              ),
+          child: Column(
+            children: [
+              Text("Dernier parcours :", style: TextStyle(fontSize: 20,color: Colors.white)),
+              SizedBox(height: 26),
+              Container(
+                width: 348,
+                height: 212,
+                child: FlutterMap(
+                  options: MapOptions(
+                    center: LatLng(48.8566, 2.3522),
+                    zoom: 12.0,
+                  ),
+                  children: [
+                    TileLayer(
+                      urlTemplate: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+                      userAgentPackageName: 'dev.fleaflet.flutter_map.example',
+                    ),
+                  ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+
+
+            Container(
+              width: 350.0,
+              height: 70.0,
+              decoration: BoxDecoration(
+                color: Color(0xFF001420),
+                borderRadius: BorderRadius.all(Radius.circular(20.0)),
+              ),
+              child : Align(
+              alignment: Alignment.center,
+              child: Text("Tous les parcours", style: TextStyle(fontSize: 20,color: Colors.white)),
+            ),
+
+            ),
+
+            Container(
+              width: 350.0,
+              height: 310.0,
+              decoration: BoxDecoration(
+                color: Color(0xFF001420),
+                borderRadius: BorderRadius.all(Radius.circular(20.0)),
+              ),
+            ),
+          ],
+        ),
+      ),
+      backgroundColor: Color(0xFF386E8F),
     );
   }
 }
