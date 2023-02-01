0da783f2f267ad988c83a4f5004a37036c5b2171
SimonL14 <simon-bcsm-11@hotmail.fr>
2023-02-01 11:28:17 +0100

    File explorer ajouté

--- lib/home.dart
+++ lib/home.dart
@@ -1,8 +1,11 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:runard/import.dart';
 import 'gpx_parse.dart';
 import 'dbhelper.dart';
+import 'import.dart';
 
 class MyHomePage extends StatelessWidget {
 
@@ -24,11 +27,11 @@ class MyHomePage extends StatelessWidget {
         ),
         backgroundColor: Color(0xFF001420),
         actions: <Widget>[
-        TextButton(
-        style: style,
-        onPressed: () {},
-        child: const Icon(Icons.dehaze),
-        ),
+          TextButton(
+            style: style,
+            onPressed: () {},
+            child: const Icon(Icons.dehaze),
+          ),
         ],
       ),
       body: Center(
@@ -58,22 +61,6 @@ class MyHomePage extends StatelessWidget {
                   ),
                 ],
               ),
-            ),
-
-
-
-            Container(
-              width: 325.0,
-              height: 70.0,
-              decoration: BoxDecoration(
-                color: Color(0xFF001420),
-                borderRadius: BorderRadius.all(Radius.circular(20.0)),
-              ),
-              child : Align(
-              alignment: Alignment.center,
-              child: Text("Tous les parcours", style: TextStyle(fontSize: 20,color: Colors.white)),
-            ),
-
             ),
 
             Container(
